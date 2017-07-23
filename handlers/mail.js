9c0146e561cb9024420f15b7e30648575a83054d
Derrick <stjohndlee@gmail.com>
2017-07-23 20:27:55 +0900

    fixing error

--- handlers/mail.js
+++ handlers/mail.js
@@ -28,7 +28,7 @@ const postmark = require('postmark');
 
 const client = new postmark.Client("7209514b-cc54-4c01-8194-d72313c18155");
 
-exports.send = async(options) => {
+exports.send = (options) => {
   const text = `${options.from} ${options.text}`;
   client.sendEmail({
     "From": "engineering@nabba.co.kr",
