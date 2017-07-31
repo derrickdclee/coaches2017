45dba5dbc4d04c74988e16b078791e452cfe80fd
Derrick <stjohndlee@gmail.com>
2017-08-01 03:11:26 +0900

    modified smooth scrolling parameters

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,10 +4,8 @@ $(document).ready(function() {
     e.preventDefault();
     const hash = this.hash;
     const height = $(hash).offset().top;
-    const navHeight = $('.navbar').outerHeight(true);
-    const finalHeight = height - navHeight * 2;
     $('html, body').animate({
-      scrollTop: finalHeight
+      scrollTop: height
     }, 600, function() {
       window.location.hash = hash;
     });
